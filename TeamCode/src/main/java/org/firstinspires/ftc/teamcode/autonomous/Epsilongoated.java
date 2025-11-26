447928364a3ded49174791890d99f3695081f74c
colin_kavanaugh <ckab3411@gmail.com>
2025-11-25 22:07:45 -0600

    9 balls intaked 0 shot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Epsilongoated.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Epsilongoated.java
@@ -4,6 +4,7 @@ import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.follower;
 
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -16,20 +17,53 @@ import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 public class Epsilongoated extends OpMode {
     private Timer pathtimer;
     private Timer OpmodeTimer;
+
+    public enum servos{
+        intake,
+    }
     public enum Pathstate {
         //start and end POS
         //Drive- movement
         //shoot
         DRIVE_STARTPOS_SHOOT_POS,
         SHOOT_PRELOAD,
-        Stafretopickspike1
+        Stafretopickspike1,
+        CArrytoscore,
+        spike2,
+        StrafetoPICK2,
+        SCORE2,
+        PARK,
+
+
 
     }
+    private PathChain drivestartpostoshootpos,
+            Stafreto1,
+            thruoghSpike1,
+            fromPickuptoscore,
+            STRAFE2,
+            Though2Spike,
+            FromSpike2TOscore,
+            PARRKRRKR;
+
+
+
+
+
     Pathstate pathstate;
     private final Pose startPose = new Pose(21.75700934579439,125.83177570093459,Math.toRadians(144));
     private final Pose shootPose = new Pose(44.41121495327103,104.5233644859813,Math.toRadians(144));
     private final Pose strafePose1 =new Pose(44.41121495327103,84.33644859813086,Math.toRadians(180));
-    private PathChain drivestartpostoshootpos,Stafretopickspike1;
+    private final Pose intkespike = new Pose(12.560747663551403,83.88785046728971,Math.toRadians(180));
+    private final Pose shootSpike1 = new Pose(44.41121495327103,104.5233644859813,Math.toRadians(144));
+    private final Pose strafePose2 =new Pose(44.18691588785046,59.88785046728972,Math.toRadians(180));
+    private final Pose inatkePike2 =new Pose(12.785046728971961,59.663551401869164,Math.toRadians(180));
+
+    private final Pose shoot2Spike =new Pose(46.205607476635514,104.29906542056075,Math.toRadians(144));
+    private final Pose score2ANDPARK =new Pose(20.859813084112147,69.30841121495328, Math.toRadians(270));
+    //private final Pose
+    //private final Pose
+
 
 
     public void buildPaths(){
@@ -38,34 +72,94 @@ public class Epsilongoated extends OpMode {
                 .addPath(new BezierLine(startPose,shootPose))
                 .setLinearHeadingInterpolation(startPose.getHeading(),shootPose.getHeading())
                 .build();
-        Stafretopickspike1 =follower.pathBuilder()
+        Stafreto1 =follower.pathBuilder()
                 .addPath(new BezierLine(shootPose,strafePose1))
                 .setLinearHeadingInterpolation(shootPose.getHeading(), strafePose1.getHeading())
                 .build();
+        thruoghSpike1 = follower.pathBuilder()
+                .addPath(new BezierLine(strafePose1, intkespike))
+                .setLinearHeadingInterpolation(strafePose1.getHeading(), intkespike.getHeading())
+                .build();
+        fromPickuptoscore =follower.pathBuilder()
+                .addPath(new BezierLine(intkespike,shootSpike1))
+                .setLinearHeadingInterpolation(intkespike.getHeading(),shootSpike1.getHeading())
+                .build();
+        STRAFE2 =follower.pathBuilder()
+                .addPath(new BezierLine(shootSpike1,strafePose2))
+                .setLinearHeadingInterpolation(shootSpike1.getHeading(),strafePose2.getHeading())
+                .build();
+        Though2Spike =follower.pathBuilder()
+                .addPath(new BezierLine(strafePose2,inatkePike2))
+                .setLinearHeadingInterpolation(strafePose2.getHeading(),inatkePike2.getHeading())
+                .build();
+        FromSpike2TOscore =follower.pathBuilder()
+                .addPath(new BezierLine(inatkePike2,shoot2Spike))
+                .setLinearHeadingInterpolation(inatkePike2.getHeading(),shoot2Spike.getHeading())
+                .build();
+        PARRKRRKR =follower.pathBuilder()
+                .addPath(new BezierLine(shoot2Spike,score2ANDPARK))
+                .setLinearHeadingInterpolation(shoot2Spike.getHeading(), score2ANDPARK.getHeading())
+                .build();
+
 
     }
     public void statePathupdate(){
         switch(pathstate){
             case DRIVE_STARTPOS_SHOOT_POS:
                 follower.followPath(drivestartpostoshootpos,true);
-                setPathstate(Pathstate.SHOOT_PRELOAD);// reste the timer and make new state
+                setPathstate(Pathstate.SHOOT_PRELOAD);// reset the timer and make new state
                 break;
             case SHOOT_PRELOAD:
                 //cheak is follower done its path ?
-                if (!follower.isBusy()&& pathtimer.getElapsedTimeSeconds() >5 ){
+                if (!follower.isBusy()&& pathtimer.getElapsedTimeSeconds() >3 ){
                     //TODO add logic to flywheel shooter
-                    follower.followPath(Stafretopickspike1, true);
+                    follower.followPath(Stafreto1, true);
                     setPathstate(Pathstate.Stafretopickspike1);
-                    telemetry.addLine("done path 1");
+                    telemetry.addLine("lines up with the spike mark");
                 }
                 break;
-            case Stafretopickspike1:
-                if (!follower.isBusy()){
-                    telemetry.addLine("going to pick up spick mark");
+
+            case  Stafretopickspike1:
+
+                if (!follower.isBusy()&& pathtimer.getElapsedTimeSeconds()>1){
+                    follower.followPath(thruoghSpike1,false );
+                    setPathstate(Pathstate.CArrytoscore);
+                    telemetry.addLine("intakeing da balls");
+
                 }
-            default:
-                telemetry.addLine("no state commanded");
                 break;
+            case CArrytoscore:
+                if(!follower.isBusy()&&pathtimer.getElapsedTimeSeconds()>2){
+                    follower.followPath(fromPickuptoscore,true);
+                    setPathstate(Pathstate.spike2);
+                    telemetry.addLine("shoots the 1st spike marks atleast should!");
+                }
+                break;
+            case spike2:
+                if(!follower.isBusy()&&pathtimer.getElapsedTimeSeconds()>2){
+                    follower.followPath(STRAFE2,true);
+                    setPathstate(Pathstate.StrafetoPICK2);
+                }
+                break;
+            case StrafetoPICK2:
+                if (!follower.isBusy()&&pathtimer.getElapsedTimeSeconds()>1){
+                    follower.followPath(Though2Spike,false);
+                setPathstate(Pathstate.SCORE2);}
+                break;
+            case SCORE2:
+                if(!follower.isBusy()&&pathtimer.getElapsedTimeSeconds()>4) {
+                    follower.followPath(FromSpike2TOscore,false);
+                    setPathstate(Pathstate.PARK);
+                }
+                    break;
+            case PARK:
+                if(!follower.isBusy()&&pathtimer.getElapsedTimeSeconds()>1){
+                    follower.followPath(PARRKRRKR,false);
+                setPathstate(Pathstate.PARK);}
+
+
+
+
         }
     }
     public void setPathstate(Pathstate newstate ){
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -19,24 +19,23 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 @Configurable
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            //.forwardZeroPowerAcceleration(-39.25)
-            .lateralZeroPowerAcceleration(-79.51)
-            .mass(10.2);
-//            .translationalPIDFCoefficients(new PIDFCoefficients(0.056, 0.0001, 0.005, 0))
-//            .headingPIDFCoefficients(new PIDFCoefficients(.8,0.01,0.001,0.01))
-//            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.004,0.000004,0.00008,0.6,0.0))
+            .mass(5)
+            .forwardZeroPowerAcceleration(-24.342792289179076)
+            .lateralZeroPowerAcceleration(-66.44318641052747)
+            .translationalPIDFCoefficients(new PIDFCoefficients(.060, 0, 0.001, 0.02))
+            .headingPIDFCoefficients(new PIDFCoefficients(0.9, 0,0.1, 0.05))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.005,0.0,0.000010,0.6,0.0010));
 
-            //.translationalPIDFCoefficients(new PIDFCoefficients(0.056, 0.000, 0.005, 0.0))
-            //.headingPIDFCoefficients(new PIDFCoefficients(1.2,0.0,0.001,0.01))
-            //.drivePIDFCoefficients(new FilteredPIDFCoefficients(0.018,0.00000,0.00008,0.6,0.0)) // p = 0.03
-            //.centripetalScaling(0.0005);
 
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             // Check issue with either wheels or wiring of motors because drive code is not working atm
             .maxPower(1)
-            .xVelocity(53.36623016117126)
-            .yVelocity(40.3781065427559)
+            .xVelocity(76.66340072511689)
+            .yVelocity(59.63964507335753)
+
+
+
 
             .rightFrontMotorName("frontRight")
             .rightRearMotorName("backRight")
@@ -47,7 +46,11 @@ public class Constants {
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static PathConstraints pathConstraints = new PathConstraints(
+            0.99,
+            100,
+            1,
+            1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
@@ -57,12 +60,12 @@ public class Constants {
                 .build();
     }
     public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(3.125) // 3.125   5.500
-            .strafePodX(-0.375) // 0.375  -3.700
+            .forwardPodY(8) // 3.125   5.500
+            .strafePodX(-4.25) // 0.375  -3.700
             .distanceUnit(DistanceUnit.INCH)
             .hardwareMapName("pinpoint")
-            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD)
             //If running into issue with pods later keep in mind the x is y and y is x, no clue why but otherwise values wouldnt work
-            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
-            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
 }
