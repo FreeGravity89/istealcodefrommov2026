acee758025be7b471327ed73131ca84ca3704f56
ImSuppressor <dylangibbs8550@gmail.com>
2025-11-30 13:54:18 -0600

    colin stupid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Epsilongoated.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Epsilongoated.java
@@ -204,6 +204,9 @@ public class Epsilongoated extends OpMode {
         OpmodeTimer = new Timer();
         servosandmotors.init(hardwareMap);
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
+        limelight.start(); // This tells Limelight to start looking!
+        limelight.pipelineSwitch(0); // Switch to pipeline number 0
         follower = Constants.createFollower(hardwareMap);
         //TODO add in any other init mechaniams
         buildPaths();
