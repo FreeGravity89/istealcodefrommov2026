32663202879c24c9b343b120a429bde8912478a8
ImSuppressor <dylangibbs8550@gmail.com>
2025-12-01 15:23:11 -0600

    limelighting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/LimelightTestes.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/LimelightTestes.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 import static com.qualcomm.hardware.limelightvision.LLResultTypes.*;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.LLStatus;
@@ -12,32 +14,22 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import java.util.List;
 
 
-public class LimelightTestes extends OpMode {
+public class LimelightTestes {
     public Limelight3A limelight;
 
-    @Override
     public void init() {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
         limelight.pipelineSwitch(0); // Switch to pipeline number 0
         limelight.start(); // This tells Limelight to start looking!
     }
-    @Override
-    public void start(){
-
-    }
-
-    @Override
     public void loop() {
-        //LLResult result = limelight.getLatestResult();
-       // if (result != null && result.isValid()) {
         LLResult llResult = limelight.getLatestResult();
         if (llResult != null && llResult.isValid()){
             //telemetry.addData("ID", llResult.get());
             List<FiducialResult> fiducials = llResult.getFiducialResults();
             for (FiducialResult fiducialResult : fiducials) {
                 int id = fiducialResult.getFiducialId();
-                telemetry.addData("IDTAG", id);
             }
         }
 
